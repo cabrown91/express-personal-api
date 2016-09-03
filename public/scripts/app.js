5ec9f9f46866f6145535bcb73578312b19f4654b
cabrown91 <cleo.aliciabrown@gmail.com>
2016-09-02 17:18:49 -0700

    add handlebars javascript/ajax to pull data into html (properly pulls in data)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,5 +3,27 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
 // your code
+$.ajax({
+  method: "GET",
+  url: "/api/profile",
+  success: onSuccess
+});
+
+function onSuccess(json) {
+  var source   = $("#profile-template").html(),
+      template = Handlebars.compile(source),
+      $profile = $("#profile-list");
+
+  var profileHtml = template({
+   name: json.name,
+   githubLink: json.githubLink,
+   githubProfileImage: json.githubProfileImage,
+   personalSiteLink: json.personalSiteLink,
+   currentCity: json.currentCity
+  });
+
+  $profile.prepend(profileHtml);
+
+}
 
 });
