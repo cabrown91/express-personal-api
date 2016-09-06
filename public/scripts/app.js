1d9d215b2203c92a42011e30b86752f05c110e1e
cabrown91 <cleo.aliciabrown@gmail.com>
2016-09-05 17:30:21 -0700

    update shows.js to fix error with export

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
+var allShows = [];
+
 $(document).ready(function(){
+
+  $profile = $("#profile-list");
 
 // your code
 $.ajax({
@@ -11,8 +15,7 @@ $.ajax({
 
 function onSuccess(json) {
   var source   = $("#profile-template").html(),
-      template = Handlebars.compile(source),
-      $profile = $("#profile-list");
+      template = Handlebars.compile(source);
 
   var profileHtml = template({
    name: json.name,
@@ -25,5 +28,45 @@ function onSuccess(json) {
   $profile.prepend(profileHtml);
 
 }
+
+$('#newShowForm').on('submit', function(e){
+  e.preventDefault();
+  console.log('new show serialized', $(this).serializeArray());
+  $.ajax({
+    method: 'POST',
+    url: '/api/shows',
+    data: $(this).serializeArray(),
+    success: newShowSuccess,
+    error: newShowError
+  });
+});
+
+function newShowSuccess(json) {
+  $('#newShowForm input').val('');
+  allShows.push(json);
+  render();
+}
+
+  $.ajax({
+    method: "DELETE",
+    url: "/api/shows/" + $(this).attr('data-id'),
+    success: deleteShowSuccess,
+    error: deleteShowError
+  });
+
+  function deleteShowSuccess(json) {
+    var show = json;
+    console.log(json);
+    var showId = show._id;
+    console.log("delete show", showId);
+
+    for(i=0; i<show.length; i++) {
+      if(show[i]._id === showId) {
+        show.splice(i, 1);
+        break;
+      }
+    }
+    render();
+  }
 
 });
