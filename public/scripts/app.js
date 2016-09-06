cb15b572e53a468697837b67d20939ad6d6ca94f
cabrown91 <cleo.aliciabrown@gmail.com>
2016-09-06 06:36:09 -0700

    add functions for post shows and delete shows. add function to render all shows to the view

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,13 @@
 console.log("Sanity Check: JS is working!");
-
-var allShows = [];
+  var $shows;
+  var allShows = [];
 
 $(document).ready(function(){
 
-  $profile = $("#profile-list");
+  $shows = $('#showsTarget');
+
+  var source = $("#shows-template").html(),
+      template = Handlebars.compile(source);
 
 // your code
 $.ajax({
@@ -15,7 +18,8 @@ $.ajax({
 
 function onSuccess(json) {
   var source   = $("#profile-template").html(),
-      template = Handlebars.compile(source);
+      template = Handlebars.compile(source),
+      $profile = $("#profile-list");
 
   var profileHtml = template({
    name: json.name,
@@ -29,44 +33,88 @@ function onSuccess(json) {
 
 }
 
+$.ajax({
+  method: "GET",
+  url: "/api/shows",
+  success: showsSuccess,
+  error: showsError
+});
+
+
 $('#newShowForm').on('submit', function(e){
   e.preventDefault();
-  console.log('new show serialized', $(this).serializeArray());
   $.ajax({
     method: 'POST',
     url: '/api/shows',
-    data: $(this).serializeArray(),
+    data: $(this).serialize(),
     success: newShowSuccess,
     error: newShowError
   });
 });
 
-function newShowSuccess(json) {
-  $('#newShowForm input').val('');
-  allShows.push(json);
-  render();
-}
-
+$shows.on('click', '.delete-btn', function(){
   $.ajax({
     method: "DELETE",
     url: "/api/shows/" + $(this).attr('data-id'),
     success: deleteShowSuccess,
     error: deleteShowError
   });
+});
 
-  function deleteShowSuccess(json) {
-    var show = json;
-    console.log(json);
-    var showId = show._id;
-    console.log("delete show", showId);
+  function render() {
+    $shows.empty();
 
-    for(i=0; i<show.length; i++) {
-      if(show[i]._id === showId) {
-        show.splice(i, 1);
-        break;
-      }
-    }
-    render();
+    var showsHtml = template({ shows: allShows });
+
+    $shows.append(showsHtml);
   }
+
+
+
+
+
+function showsSuccess(json) {
+  allShows = json;
+  render();
+}
+
+function showsError(error) {
+  console.log("error", error);
+  $('#showsTarget').text('Failed to load shows. Is the server working?');
+}
+
+function newShowSuccess(json) {
+  $('input').val('');
+  allShows.push(json);
+  render();
+}
+
+function newShowError(error) {
+  console.log("Failed to create show", error);
+}
+
+
+function deleteShowSuccess(json) {
+  var show = json;
+  var showId = show._id;
+  console.log("delete show", showId);
+
+  for(i=0; i<show.length; i++) {
+    if(show[i]._id === showId) {
+      show.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+function deleteShowError(err) {
+  console.log("Failed to delete show", err);
+}
+
+
+
+
+
 
 });
